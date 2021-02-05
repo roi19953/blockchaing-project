a2769d267a55a113d70b1aabd51e05a81eeb1aaf
Roy Ben Yosef <roy.ben.yosef@intel.com>
2021-02-05 16:31:27 +0200

    print data

--- src/client.js
+++ src/client.js
@@ -77,7 +77,7 @@ const createBatch = (transactions) => {
 };
 
 /* This batch creates a new game */
-const batchToSend = createBatch([createTransaction(`TelAviv,createCity,0`)]);
+const batchToSend = createBatch([createTransaction(`TelAviv,createDriver,roy`)]);
 console.log("enter here1")
 
 const batchListBytes = protobuf.BatchList.encode({
