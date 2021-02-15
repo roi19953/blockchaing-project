ac33342c30ed4ee135d18218b6d13e20caf959c8
roi19953 <roi19953@gmail.com>
2021-02-15 20:46:42 +0200

    commit from bash

--- src/client.js
+++ src/client.js
@@ -77,7 +77,7 @@ const createBatch = (transactions) => {
 };
 
 /* This batch creates a new game */
-const batchToSend = createBatch([createTransaction(`Omri'sGame,take,3`)]);
+const batchToSend = createBatch([createTransaction(`Omri'sGame,create,0`)]);
 
 const batchListBytes = protobuf.BatchList.encode({
   batches: [batchToSend],
