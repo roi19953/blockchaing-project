36e5fb55e36a11e704185b10323404597d87859a
Roy Ben Yosef <roy.ben.yosef@intel.com>
2021-02-17 20:32:48 +0200

    testing batch changes

--- src/test1.js
+++ src/test1.js
@@ -1,4 +1,17 @@
-var MyArgs = process.argv.slice(2).toString(); 
-const [gameName, action, space] = MyArgs.split(",");
-console.log("my args: "+ MyArgs.split(","))
-console.log(action)
+var prompt = require('prompt');
+ 
+prompt.start();
+
+var thisResult = "";
+
+prompt.get(['username', 'email'], function (err, result) {
+  console.log('Command-line input received:');
+  thisResult = result.username
+});
+
+console.log(thisResult)
+
+// const {username, email} = await prompt.get(['username', 'email']);
+
+// const {username, email} = await prompt.get(['username', 'email']);
+
